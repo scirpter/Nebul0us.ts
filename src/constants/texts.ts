e1f43a55622910e36af976d54291d08fcf8cbbf4
Sascha <nyaanity@gmail.com>
2022-08-24 13:35:25 +0200

    deprecated

--- src/constants/texts.ts
+++
@@ -1,4 +0,0 @@
-export const SCRIPT_EXPIRED =
-    "No permission. Join our Discord server using this link https://discord.com/invite/YjnAQq3BRN and purchase a license to use this script.";
-export const F_BIN_CHUNK = "444f4e4f5444454c45544554484953.chunk";
-export const RC4_KEY = "NYAwPU@[M?V`Z_66@AqK<q<{";
